fbdfff32d1d2247af474181266f97ee3a2417d90
Zainab-Mhjoub <113207077+Zainab-Mhjoub@users.noreply.github.com>
2023-02-28 11:56:42 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// window.alert("hell from js");
+// console.log("welcome");
+// console.error("error not limite");
+// console.table(["asma", "zainab"]);
+// console.log("hello from %cjs %cfile", "color:red; font-size:20px", "color:blue; font-size:20px");
