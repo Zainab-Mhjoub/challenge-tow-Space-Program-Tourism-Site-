2df3f7584e737a1f0e53120b1018dbafa28822ca
Zainab-Mhjoub <113207077+Zainab-Mhjoub@users.noreply.github.com>
2023-04-09 00:24:01 +0100

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-// window.alert("hell from js");
-// console.log("welcome");
-// console.error("error not limite");
-// console.table(["asma", "zainab"]);
-// console.log("hello from %cjs %cfile", "color:red; font-size:20px", "color:blue; font-size:20px");
